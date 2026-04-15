7c7206e3cc04cd873b92c0906d5a9a5c4a80cc90
Caio-Andrade-Santos <cavians2015@gmail.com>
2026-04-15 19:34:50 -0300

    Update TP02Ex10.java

--- tp02ex10/TP02Ex10.java
+++ tp02ex10/TP02Ex10.java
@@ -3,6 +3,13 @@ package tp02ex10;
 import java.util.Scanner;
 import java.util.Locale;
 
+/*
+    Nomes: Caio Andrade, Alexandre Zampieri
+    10. Entrar com uma matriz de ordem MxM, onde a ordem também será escolhida pelo usuário,
+sendo que no máximo será de ordem 10 e quadrática. Após a digitação dos elementos,
+calcular e exibir a matriz inversa. Exibir as matrizes na tela, sob a forma matricial (linhas x
+colunas).
+*/
 public class TP02Ex10 {
 
     public static void CriaMatriz() {
